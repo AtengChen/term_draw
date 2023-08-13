e76d9eeeee9fe97e669fd4357d0bec4a41c6372d
Aten Chen <109785614+AtengChen@users.noreply.github.com>
2023-08-13 15:49:40 +0800

    Update term_draw.py

--- term_draw.py
+++ term_draw.py
@@ -4,7 +4,10 @@ from PIL.Image import open as open_im
 from time import sleep
 
 
-im = open_im(input())
+im = open_im(input("File: "))
+shade_flag = bool(input("Shade? [<blank>(No) / <no-blank>(Yes)] "))
+alg_num = int(input("Alg: "))
+
 im = im.convert("RGB")
 w, h = im.size
 tw, th = get_terminal_size()
@@ -21,12 +24,21 @@ init()
 
 
 
-def color(r, g, b):
-    return f"\033[38;2;{r};{g};{b}m██\033[0m"
+def color(r, g, b, shade=False, alg_num=3):
+    if shade:
+        char = [" ", "░", "▒", "▓", "█"]
+    else:
+        char = ["█"]
+    algs = [lambda: (r + g + b) / 3,
+            lambda: 0.2126 * r + 0.7152 * g + 0.0722 * b,
+            lambda: max(r, g, b),
+            lambda: min(r, g, b)]
+    a = 255 - algs[alg_num]()
+    return f"\033[38;2;{r};{g};{b}m{char[round(a / 255 * len(char)) - 1] * 2}\033[0m"
 
 for i in range(h2):
     for j in range(w2):
-        print(color(*im.getpixel((j, i))), end="")
+        print(color(*im.getpixel((j, i)), shade=shade_flag, alg_num=alg_num), end="")
     print()
     sleep(1 / 24)
 
